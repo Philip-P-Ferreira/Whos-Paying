805eb26e046313f967a89fc06c72525d82e40799
Blaine Mitchell <blaine.z.mitchell@vanderbilt.edu>
2021-10-10 01:10:58 -0500

    implemented api

--- webapp/src/apis/api.ts
+++ webapp/src/apis/api.ts
@@ -5,19 +5,115 @@ export async function loginApi(username: string, password: string) {
   try {
     const fullUrl = API_PREFIX + "/api/login?username=" + username + "&password=" + password
     const response = await fetch(fullUrl)
-    console.log(response)
 
-    if (await response.text() === "Logged in") {
-      return true
-    }
-    return false
+    return await response.text() === "Logged in"
   } catch (e) {
     console.error(e)
     return false
   }
+
+}
+
+export async function createLobbyApi(bill: number): Promise<String | null> {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/create-lobby?bill=" + bill
+    const response = await fetch(fullUrl)
+
+    return await response.text()
+  } catch(e) {
+    console.error(e)
+    return null
+  }
+
+}
+
+export async function joinLobbyApi(lobbyCode: string) {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/join-lobby/" + lobbyCode
+    const response = await fetch(fullUrl)
+
+    return await response.text() === "Joining lobby code " + lobbyCode
+  } catch(e) {
+    console.error(e)
+    return false
+  }
+
+}
+
+export async function lobbyStateApi(lobbyCode: string): Promise<JSON | null> {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/lobby-state/" + lobbyCode
+    const response = await fetch(fullUrl)
+
+    return await response.json()
+  } catch(e) {
+    console.error(e)
+    return null
+  }
+
+}
+
+export async function leaveLobbyApi() {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/leave-lobby"
+    const response = await fetch(fullUrl)
+
+    return await response.text() === ""
+  } catch(e) {
+    console.error(e)
+    return false
+  }
+
+}
+
+export async function endLobbyApi() {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/end-lobby"
+    const response = await fetch(fullUrl)
+
+    return await response.text() === ""
+  } catch(e) {
+    console.error(e)
+    return false
+  }
+
+}
+
+export async function readyApi(lobbyCode: string) {
+  
+  try {
+    const fullUrl = API_PREFIX + "/api/ready/" + lobbyCode
+    const response = await fetch(fullUrl)
+
+    return await response.text() === "Ready"
+  } catch(e) {
+    console.error(e)
+    return false
+  }
+
+}
+
+export async function countdownGameApi(lobbyCode: string): Promise<JSON | null> {
+  
+  try {
+    const fullUrl = API_PREFIX + "/api/countdown-game/" + lobbyCode
+    const response = await fetch(fullUrl)
+
+    return await response.json()
+  } catch(e) {
+    console.error(e)
+    return null
+  }
+
 }
 
 export async function balanceApi(): Promise<string | null> {
+
   try {
     const fullUrl = API_PREFIX + "/api/balance"
     const response = await fetch(fullUrl)
@@ -28,4 +124,18 @@ export async function balanceApi(): Promise<string | null> {
     return null
   }
 
+}
+
+export async function countdownSubmitApi(lobbyCode: string, solution: number) {
+
+  try {
+    const fullUrl = API_PREFIX + "/api/countdown-submit/" + lobbyCode + "?solution=" + solution
+    const response = await fetch(fullUrl)
+
+    return await response.text() === ""
+  } catch(e) {
+    console.error(e)
+    return false
+  }
+  
 }
